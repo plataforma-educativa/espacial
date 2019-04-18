b2ce7fbd7630bfcab2f931c456c5ded62a19dff2
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-04-18 18:41:11 -0300

    Detalle del error incluye el nombre del excepción

--- src/test/java/espacial/test/Condicion.java
+++ src/test/java/espacial/test/Condicion.java
@@ -29,7 +29,8 @@ public abstract class Condicion {
         return new StringBuilder()
                     .append(describir())
                     .append('\n')
-                    .append("Detalle: ")
+                    .append(e.getClass().getSimpleName())
+                    .append(": ")
                     .append(e.getMessage())
                     .toString();
     }
