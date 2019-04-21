779f056248d61a2cf8d308ce264353ef90b0a82d
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-04-21 11:03:32 -0300

    Documentación de Casillero

--- src/main/java/espacial/Casillero.java
+++ src/main/java/espacial/Casillero.java
@@ -3,8 +3,8 @@ package espacial;
 import espacial.piezas.Pieza;
 
 /**
- * El Tablero del Juego está compuesto por un conjunto de Casilleros.
- * Cada Casillero podrá tener a lo sumo una Pieza del Juego.
+ * El Tablero del Juego está compuesto por un conjunto de Casilleros. Cada
+ * Casillero podrá estar ocupado por una Pieza del Juego.
  * 
  * @author Mariano Tugnarelli
  *
@@ -13,28 +13,41 @@ public class Casillero {
 
     private final Coordenada coordenada;
     private Pieza pieza = null;
-    
+
     public Casillero(int fila, int columna) {
         coordenada = new Coordenada(fila, columna);
     }
-    
+
+    /**
+     * @return EspectroEspacial de la Pieza que ocupa el Casillero, o
+     *         EspectroEspacial.VACIO en caso de estar desocupado.
+     */
     public EspectroEspacial escanear() {
-        
+
         return pieza != null ? pieza.escanear() : EspectroEspacial.VACIO;
     }
 
     /**
-     * pre : el Casillero no está ocupado. 'pieza' no está 
-     *       colocada en otro Casillero.
-     *       
-     * @param pieza
+     * @pre el Casillero no está ocupado.
+     * @pre {@code unaPieza} no está asociada a otro Casillero.
+     * @post {@code unPieza} queda asociada al Casillero actual.
+     * 
+     * @param unaPieza
      */
-    public void ocuparCon(Pieza pieza) {
-        
-        this.pieza = pieza;
+    public void ocuparCon(Pieza unaPieza) {
+
+        this.pieza = unaPieza;
         this.pieza.posicionar(coordenada);
     }
 
+    /**
+     * @pre el Casillero está ocupado.
+     * @pre {@code destino} no está ocupado.
+     * @post desocupa el Casillero y utiliza esa Pieza para ocupar el Casillero
+     *       {@code destino}.
+     * 
+     * @param destino
+     */
     public void moverPiezaA(Casillero destino) {
 
         Pieza piezaMovida = this.pieza;
@@ -42,9 +55,12 @@ public class Casillero {
         destino.ocuparCon(piezaMovida);
     }
 
+    /**
+     * @pre el Casillero está ocupado por una Pieza.
+     * @post remueve la Pieza del Casillero.
+     */
     public void desocupar() {
-        
+
         this.pieza = null;
     }
-
 }
