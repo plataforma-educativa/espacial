b9823f383072a1a2a35b3d88c92a3307b883f04a
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-05-12 19:39:28 -0300

    Excepción serializable con número de versión

--- src/main/java/espacial/excepciones/LaNaveNoEstaEnLaBase.java
+++ src/main/java/espacial/excepciones/LaNaveNoEstaEnLaBase.java
@@ -2,6 +2,8 @@ package espacial.excepciones;
 
 public class LaNaveNoEstaEnLaBase extends ErrorEnLaBatallaEspacial {
 
+    private static final long serialVersionUID = -3660738993645747467L;
+
     public LaNaveNoEstaEnLaBase() {
 
         super("la Nave no está en la Base");
