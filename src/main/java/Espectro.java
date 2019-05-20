65ef62b956b9a7098661b219a856a6102fcdca41
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-05-19 23:16:21 -0300

    Documentación del Espectro

--- src/main/java/Espectro.java
+++ src/main/java/Espectro.java
@@ -1,4 +1,11 @@
 
+/**
+ * El Espectro indica el resultado de haber realizar un escaneo con el Radar sobre un casillero.
+ * Indica la presencia o ausencia de algún tipo de Pieza.
+ *
+ * @author Mariano Tugnarelli
+ *
+ */
 public enum Espectro {
 
     VACIO,
