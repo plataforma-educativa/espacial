4559cdd9c2c4f2664421c450739675fe64f2b45e
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-06-09 17:42:44 -0300

    Refactoring: tests sobre Contenedores agrupados

--- src/test/java/espacial/piezas/ContenedorDeSustanciaTest.java
+++ src/test/java/espacial/piezas/ContenedorDeSustanciaTest.java
@@ -1,201 +1,56 @@
 package espacial.piezas;
 
-import espacial.EspectroEspacial;
 import espacial.SustanciaEspacial;
-import espacial.excepciones.ExcedeLaCapacidadDeCarga;
-import espacial.excepciones.ExcedeLaCargaDisponible;
-import espacial.test.Ejecutable;
-import espacial.test.Postcondicion;
-import espacial.test.Precondicion;
-import org.junit.jupiter.api.Test;
+import espacial.test.TestDeContrato;
+import org.junit.jupiter.api.Nested;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+public class ContenedorDeSustanciaTest implements TestDeContrato {
 
-public abstract class ContenedorDeSustanciaTest<T extends ContenedorDeSustancia> extends TestDeContratoSobrePieza<T> {
+    @Nested
+    public class AntimateriaTest extends TestDeContratoSobreContenedor<ContenedorDeAntimateria> {
 
-    private T unContenedor;
+        @Override
+        protected SustanciaEspacial sustanciaAlmacenada() {
 
-    protected abstract SustanciaEspacial sustanciaAlmacenada();
+            return SustanciaEspacial.ANTIMATERIA;
+        }
 
-    @Override
-    public EspectroEspacial espectroEsperado() {
+        @Override
+        protected ContenedorDeAntimateria piezaCreada() {
 
-        return EspectroEspacial.CONTENEDOR;
+            return new ContenedorDeAntimateria();
+        }
     }
 
-    @Override
-    public Postcondicion laNaveEspacialFueNotificadaDelChoque() {
+    @Nested
+    public class MetalTest extends TestDeContratoSobreContenedor<ContenedorDeMetal> {
 
-        return postcondicion(() -> verify(NAVE_ESPACIAL).chocoContraUnContenedor());
+        @Override
+        protected SustanciaEspacial sustanciaAlmacenada() {
+
+            return SustanciaEspacial.METAL;
+        }
+
+        @Override
+        protected ContenedorDeMetal piezaCreada() {
+
+            return new ContenedorDeMetal();
+        }
     }
 
-    @Test
-    public void obtenerPuntos() {
+    @Nested
+    public class CristalTest extends TestDeContratoSobreContenedor<ContenedorDeCristal> {
 
-        dadoQue(fueCreadoUnContenedor());
+        @Override
+        protected SustanciaEspacial sustanciaAlmacenada() {
 
-        comprobarQue(losPuntosInicialesDeUnContenedorDeSustanciaSonCorrectos());
-    }
+            return SustanciaEspacial.CRISTAL;
+        }
 
-    private Precondicion fueCreadoUnContenedor() {
+        @Override
+        protected ContenedorDeCristal piezaCreada() {
 
-        return precondicion(() -> unContenedor = piezaCreada());
-    }
-
-    private Postcondicion losPuntosInicialesDeUnContenedorDeSustanciaSonCorrectos() {
-
-        return postcondicion(() -> assertThat(unContenedor.obtenerPuntos()).as("puntos").isEqualTo(50));
-    }
-
-    @Test
-    public void recibirUnaCarga() {
-
-        final int cantidad = 65;
-
-        dadoQue(fueCreadoUnContenedor());
-
-        unContenedor.recibir(sustanciaAlmacenada().por(cantidad));
-
-        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidad));
-    }
-
-    private Postcondicion unContenedorTiene(SustanciaEspacial sustanciaEsperada, int cantidadEsperada) {
-
-        return postcondicion(() ->
-
-                assertThat(unContenedor.buscar(sustanciaEsperada))
-                        .as("buscar(" + sustanciaEsperada + ")")
-                        .isEqualTo(cantidadEsperada)
-        );
-    }
-
-    @Test
-    public void recibirMultiplesCargas() {
-
-        final int cantidadInicial = 20;
-        final int cantidad = 90;
-
-        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
-
-        unContenedor.recibir(sustanciaAlmacenada().por(cantidad));
-
-        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadInicial + cantidad));
-    }
-
-    private Precondicion fueCreadoUnContenedorDeSustanciaRecibiendo(int cantidadInicial) {
-
-        return precondicion(() -> {
-
-            unContenedor = piezaCreada();
-            unContenedor.recibir(sustanciaAlmacenada().por(cantidadInicial));
-        });
-    }
-
-    @Test
-    public void recibirLaCargaMaxima() {
-
-        final int cantidadMaxima = 1000;
-
-        dadoQue(fueCreadoUnContenedor());
-
-        unContenedor.recibir(sustanciaAlmacenada().por(cantidadMaxima));
-
-        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadMaxima));
-    }
-
-    @Test
-    public void recibirCargaExcediendoLaCapacidad() {
-
-        final int cantidadExcedida = 1001;
-
-        dadoQue(fueCreadoUnContenedor());
-
-        comprobarQue(generaExcepcionPorqueExcedeLaCapacidadDeCarga(() ->
-
-                unContenedor.recibir(sustanciaAlmacenada().por(cantidadExcedida)))
-        );
-    }
-
-    private Postcondicion generaExcepcionPorqueExcedeLaCapacidadDeCarga(Ejecutable ejecutable) {
-
-        return postcondicion(() ->
-
-                assertThatThrownBy(ejecutable::ejecutar)
-                        .as("excepción generada")
-                        .isInstanceOf(ExcedeLaCapacidadDeCarga.class)
-        );
-    }
-
-    @Test
-    public void extraerUnaCarga() {
-
-        final int cantidadInicial = 90;
-        final int cantidad = 10;
-
-        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
-
-        unContenedor.entregar(sustanciaAlmacenada().por(cantidad));
-
-        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadInicial - cantidad));
-    }
-
-    @Test
-    public void extraerMultiplesCargas() {
-
-        final int cantidadInicial = 500;
-        final int cantidadExtraida = 343;
-        final int cantidad = 132;
-
-        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
-        dadoQue(fueExtraidaDeUnContenedorDeSustanciaUnaCarga(cantidadExtraida));
-
-        unContenedor.entregar(sustanciaAlmacenada().por(cantidad));
-
-        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadInicial - cantidadExtraida - cantidad));
-    }
-
-    @Test
-    public void extraerExcediendoLaCargaDisponible() {
-
-        final int cantidadInicial = 100;
-        final int cantidad = 101;
-
-        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
-
-        comprobarQue(generaExcepcionPorqueExcedeLaCargaDisponible(() ->
-
-                unContenedor.entregar(sustanciaAlmacenada().por(cantidad)))
-        );
-    }
-
-    @Test
-    public void extraerTodaLaCargaDisponible() {
-
-        final int cantidadInicial = 345;
-
-        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
-
-        unContenedor.entregar(sustanciaAlmacenada().por(cantidadInicial));
-
-        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), 0));
-    }
-
-    private Precondicion fueExtraidaDeUnContenedorDeSustanciaUnaCarga(int cantidadExtraida) {
-
-        return precondicion(() ->
-
-                unContenedor.entregar(sustanciaAlmacenada().por(cantidadExtraida))
-        );
-    }
-
-    private Postcondicion generaExcepcionPorqueExcedeLaCargaDisponible(Ejecutable ejecutable) {
-
-        return postcondicion(() ->
-
-                assertThatThrownBy(ejecutable::ejecutar)
-                        .as("excepción generada")
-                        .isInstanceOf(ExcedeLaCargaDisponible.class)
-        );
+            return new ContenedorDeCristal();
+        }
     }
 }
---
+++ src/test/java/espacial/piezas/TestDeContratoSobreContenedor.java
@@ -0,0 +1,201 @@
+package espacial.piezas;
+
+import espacial.EspectroEspacial;
+import espacial.SustanciaEspacial;
+import espacial.excepciones.ExcedeLaCapacidadDeCarga;
+import espacial.excepciones.ExcedeLaCargaDisponible;
+import espacial.test.Ejecutable;
+import espacial.test.Postcondicion;
+import espacial.test.Precondicion;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public abstract class TestDeContratoSobreContenedor<T extends ContenedorDeSustancia> extends TestDeContratoSobrePieza<T> {
+
+    private T unContenedor;
+
+    protected abstract SustanciaEspacial sustanciaAlmacenada();
+
+    @Override
+    public EspectroEspacial espectroEsperado() {
+
+        return EspectroEspacial.CONTENEDOR;
+    }
+
+    @Override
+    public Postcondicion laNaveEspacialFueNotificadaDelChoque() {
+
+        return postcondicion(() -> verify(NAVE_ESPACIAL).chocoContraUnContenedor());
+    }
+
+    @Test
+    public void obtenerPuntos() {
+
+        dadoQue(fueCreadoUnContenedor());
+
+        comprobarQue(losPuntosInicialesDeUnContenedorDeSustanciaSonCorrectos());
+    }
+
+    private Precondicion fueCreadoUnContenedor() {
+
+        return precondicion(() -> unContenedor = piezaCreada());
+    }
+
+    private Postcondicion losPuntosInicialesDeUnContenedorDeSustanciaSonCorrectos() {
+
+        return postcondicion(() -> assertThat(unContenedor.obtenerPuntos()).as("puntos").isEqualTo(50));
+    }
+
+    @Test
+    public void recibirUnaCarga() {
+
+        final int cantidad = 65;
+
+        dadoQue(fueCreadoUnContenedor());
+
+        unContenedor.recibir(sustanciaAlmacenada().por(cantidad));
+
+        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidad));
+    }
+
+    private Postcondicion unContenedorTiene(SustanciaEspacial sustanciaEsperada, int cantidadEsperada) {
+
+        return postcondicion(() ->
+
+                assertThat(unContenedor.buscar(sustanciaEsperada))
+                        .as("buscar(" + sustanciaEsperada + ")")
+                        .isEqualTo(cantidadEsperada)
+        );
+    }
+
+    @Test
+    public void recibirMultiplesCargas() {
+
+        final int cantidadInicial = 20;
+        final int cantidad = 90;
+
+        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
+
+        unContenedor.recibir(sustanciaAlmacenada().por(cantidad));
+
+        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadInicial + cantidad));
+    }
+
+    private Precondicion fueCreadoUnContenedorDeSustanciaRecibiendo(int cantidadInicial) {
+
+        return precondicion(() -> {
+
+            unContenedor = piezaCreada();
+            unContenedor.recibir(sustanciaAlmacenada().por(cantidadInicial));
+        });
+    }
+
+    @Test
+    public void recibirLaCargaMaxima() {
+
+        final int cantidadMaxima = 1000;
+
+        dadoQue(fueCreadoUnContenedor());
+
+        unContenedor.recibir(sustanciaAlmacenada().por(cantidadMaxima));
+
+        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadMaxima));
+    }
+
+    @Test
+    public void recibirCargaExcediendoLaCapacidad() {
+
+        final int cantidadExcedida = 1001;
+
+        dadoQue(fueCreadoUnContenedor());
+
+        comprobarQue(generaExcepcionPorqueExcedeLaCapacidadDeCarga(() ->
+
+                unContenedor.recibir(sustanciaAlmacenada().por(cantidadExcedida)))
+        );
+    }
+
+    private Postcondicion generaExcepcionPorqueExcedeLaCapacidadDeCarga(Ejecutable ejecutable) {
+
+        return postcondicion(() ->
+
+                assertThatThrownBy(ejecutable::ejecutar)
+                        .as("excepción generada")
+                        .isInstanceOf(ExcedeLaCapacidadDeCarga.class)
+        );
+    }
+
+    @Test
+    public void extraerUnaCarga() {
+
+        final int cantidadInicial = 90;
+        final int cantidad = 10;
+
+        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
+
+        unContenedor.entregar(sustanciaAlmacenada().por(cantidad));
+
+        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadInicial - cantidad));
+    }
+
+    @Test
+    public void extraerMultiplesCargas() {
+
+        final int cantidadInicial = 500;
+        final int cantidadExtraida = 343;
+        final int cantidad = 132;
+
+        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
+        dadoQue(fueExtraidaDeUnContenedorDeSustanciaUnaCarga(cantidadExtraida));
+
+        unContenedor.entregar(sustanciaAlmacenada().por(cantidad));
+
+        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), cantidadInicial - cantidadExtraida - cantidad));
+    }
+
+    @Test
+    public void extraerExcediendoLaCargaDisponible() {
+
+        final int cantidadInicial = 100;
+        final int cantidad = 101;
+
+        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
+
+        comprobarQue(generaExcepcionPorqueExcedeLaCargaDisponible(() ->
+
+                unContenedor.entregar(sustanciaAlmacenada().por(cantidad)))
+        );
+    }
+
+    @Test
+    public void extraerTodaLaCargaDisponible() {
+
+        final int cantidadInicial = 345;
+
+        dadoQue(fueCreadoUnContenedorDeSustanciaRecibiendo(cantidadInicial));
+
+        unContenedor.entregar(sustanciaAlmacenada().por(cantidadInicial));
+
+        comprobarQue(unContenedorTiene(sustanciaAlmacenada(), 0));
+    }
+
+    private Precondicion fueExtraidaDeUnContenedorDeSustanciaUnaCarga(int cantidadExtraida) {
+
+        return precondicion(() ->
+
+                unContenedor.entregar(sustanciaAlmacenada().por(cantidadExtraida))
+        );
+    }
+
+    private Postcondicion generaExcepcionPorqueExcedeLaCargaDisponible(Ejecutable ejecutable) {
+
+        return postcondicion(() ->
+
+                assertThatThrownBy(ejecutable::ejecutar)
+                        .as("excepción generada")
+                        .isInstanceOf(ExcedeLaCargaDisponible.class)
+        );
+    }
+}
