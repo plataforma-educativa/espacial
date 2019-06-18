bf303a17ca1b525c4b2dc516c5fe2bffe6c2c86a
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-06-17 21:49:36 -0300

    Prueba unitaria para el Casillero Interior

---
+++ src/test/java/espacial/tableros/CasilleroInteriorTest.java
@@ -0,0 +1,177 @@
+package espacial.tableros;
+
+import espacial.Ataque;
+import espacial.Casillero;
+import espacial.EspectroEspacial;
+import espacial.Pieza;
+import espacial.SustanciaEspacial;
+import espacial.Tablero;
+import espacial.test.Postcondicion;
+import espacial.test.Precondicion;
+import espacial.test.TestDeContrato;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CasilleroInteriorTest implements TestDeContrato {
+
+    private final Tablero TABLERO = mock(Tablero.class, "TABLERO");
+    private final Pieza UNA_PIEZA = mock(Pieza.class, "UNA_PIEZA");
+    private final Casillero OTRO_CASILLERO = mock(Casillero.class, "OTRO_CASILLERO");
+    private final Ataque UN_ATAQUE = mock(Ataque.class, "UN_ATAQUE");
+
+    private final EstadoDelCasillero ESTADO = mock(EstadoDelCasillero.class, "ESTADO");
+    private final EspectroEspacial ESPECTRO_ESCANEADO = EspectroEspacial.BASE;
+    private final int CANTIDAD_DE_SUSTANCIA_ENCONTRADA = 345;
+    private final Pieza PIEZA_DEL_CASILLERO = mock(Pieza.class, "PIEZA_DEL_CASILLERO");
+
+    private CasilleroInterior unCasilleroInterior;
+
+    @Test
+    void escanear() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        comprobarQue(delegaEnElEstadoLaOperacionEscanear());
+    }
+
+    private Precondicion fueCreadoUnCasilleroInteriorCon(EstadoDelCasillero estado) {
+
+        return precondicion(() -> {
+
+            unCasilleroInterior = new CasilleroInterior(TABLERO, 0, 0);
+            unCasilleroInterior.cambiarA(ESTADO);
+            when(ESTADO.alEscanear()).thenReturn(ESPECTRO_ESCANEADO);
+            when(ESTADO.alBuscar(any(SustanciaEspacial.class))).thenReturn(CANTIDAD_DE_SUSTANCIA_ENCONTRADA);
+            when(ESTADO.alObtenerPieza()).thenReturn(PIEZA_DEL_CASILLERO);
+        });
+    }
+
+    private Postcondicion delegaEnElEstadoLaOperacionEscanear() {
+
+        return postcondicion(() ->
+
+                assertThat(unCasilleroInterior.escanear())
+                        .as("escanear()")
+                        .isEqualTo(ESPECTRO_ESCANEADO)
+        );
+    }
+
+    @Test
+    void buscar() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        comprobarQue(delegaEnElEstadoLaOperacionBuscar());
+    }
+
+    private Postcondicion delegaEnElEstadoLaOperacionBuscar() {
+
+        return postcondicion(() ->
+
+                assertThat(unCasilleroInterior.buscar(SustanciaEspacial.METAL))
+                        .as("buscar(SustanciaEspacial)")
+                        .isEqualTo(CANTIDAD_DE_SUSTANCIA_ENCONTRADA)
+        );
+    }
+
+    @Test
+    void ocuparCon() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        unCasilleroInterior.ocuparCon(UNA_PIEZA);
+
+        comprobarQue(delegoEnElEstadoLaOperacionOcuparCon());
+        comprobarQue(unaPiezaEsNotificadaQueFueColocadaEnElCasillero());
+    }
+
+    private Postcondicion delegoEnElEstadoLaOperacionOcuparCon() {
+
+        return postcondicion(() -> verify(ESTADO).alOcuparCon(UNA_PIEZA));
+    }
+
+    private Postcondicion unaPiezaEsNotificadaQueFueColocadaEnElCasillero() {
+
+        return postcondicion(() -> verify(UNA_PIEZA).fueColocadaEn(unCasilleroInterior));
+    }
+
+    @Test
+    void moverPiezaA() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        unCasilleroInterior.moverPiezaA(OTRO_CASILLERO);
+
+        comprobarQue(delegoEnElEstadoLaOperacionMoverPiezaA());
+    }
+
+    private Postcondicion delegoEnElEstadoLaOperacionMoverPiezaA() {
+
+        return postcondicion(() -> verify(ESTADO).alMoverPiezaA(OTRO_CASILLERO));
+    }
+
+    @Test
+    void recibirPiezaDesde() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        unCasilleroInterior.recibirPiezaDesde(OTRO_CASILLERO);
+
+        comprobarQue(delegoEnElEstadoLaOperacionRecibirPiezaDesde());
+    }
+
+    private Postcondicion delegoEnElEstadoLaOperacionRecibirPiezaDesde() {
+
+        return postcondicion(() -> verify(ESTADO).alRecibirPiezaDesde(OTRO_CASILLERO));
+    }
+
+    @Test
+    void desocupar() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        unCasilleroInterior.desocupar();
+
+        comprobarQue(delegoEnElEstadoLaOperacionDesodupar());
+    }
+
+    private Postcondicion delegoEnElEstadoLaOperacionDesodupar() {
+
+        return postcondicion(() -> verify(ESTADO).alDesocupar());
+    }
+
+    @Test
+    void obtenerPieza() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        comprobarQue(delegoEnElEstadoLaOperacionObtenerPieza());
+    }
+
+    private Postcondicion delegoEnElEstadoLaOperacionObtenerPieza() {
+
+        return postcondicion(() ->
+
+                assertThat(unCasilleroInterior.obtenerPieza())
+                        .as("obtenerPieza()")
+                        .isEqualTo(PIEZA_DEL_CASILLERO)
+        );
+    }
+
+    @Test
+    void fueAtacadoCon() {
+
+        dadoQue(fueCreadoUnCasilleroInteriorCon(ESTADO));
+
+        unCasilleroInterior.fueAtacadoCon(UN_ATAQUE);
+
+        comprobarQue(delegoEnElEstadoLaOperacionFueAtacadoCon());
+    }
+
+    private Postcondicion delegoEnElEstadoLaOperacionFueAtacadoCon() {
+
+        return postcondicion(() -> verify(ESTADO).alSerAtacadoCon(UN_ATAQUE));
+    }
+}
