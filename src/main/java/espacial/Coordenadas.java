c57320378fc091b3e528e25e55c8132b616db54b
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-07-09 18:18:16 -0300

    Coordenadas numeradas en espiral

--- src/main/java/espacial/Coordenadas.java
+++ src/main/java/espacial/Coordenadas.java
@@ -6,12 +6,12 @@ public class Coordenadas {
 
     private final int fila;
     private final int columna;
-    
+
     public static Coordenadas con(int fila, int columna) {
 
         return new Coordenadas(fila, columna);
     }
-    
+
     private Coordenadas(int fila, int columna) {
 
         this.fila = fila;
@@ -19,34 +19,34 @@ public class Coordenadas {
     }
 
     public int obtenerFila() {
-        
+
         return fila;
     }
 
     public int obtenerColumna() {
-        
+
         return columna;
     }
 
     @Override
     public boolean equals(Object otroObjeto) {
-        
+
         boolean iguales = (this == otroObjeto);
-        
+
         if (!iguales && otroObjeto instanceof Coordenadas) {
-            
+
             Coordenadas otraCoordenadas = (Coordenadas) otroObjeto;
-            
+
             iguales = (this.fila == otraCoordenadas.fila) &&
-                      (this.columna == otraCoordenadas.columna);
+                    (this.columna == otraCoordenadas.columna);
         }
-        
+
         return iguales;
     }
-    
+
     @Override
     public int hashCode() {
-        
+
         return Objects.hash(fila, columna);
     }
 
@@ -54,5 +54,51 @@ public class Coordenadas {
     public String toString() {
 
         return "[" + fila + "][" + columna + "]";
+    }
+
+    /**
+     * @return número que identifica la coordenada asignada con un patrón en espiral, iniciando en [0][0].
+     *
+     *      -5  -4  -3  -2  -1  [0]  1   2   3   4   5   6
+     *
+     *  5  100  99  98  97  96  95  94  93  92  91  90
+     *  4  101  64  63  62  61  60  59  58  57  56  89
+     *  3  102  65  36  35  34  33  32  31  30  55  88 129
+     *  2  103  66  37  16  15  14  13  12  29  54  87 128
+     *  1  104  67  38  17   4   3   2  11  28  53  86 127
+     * [0] 105  68  39  18   5  [0]  1  10  27  52  85 126
+     * -1  106  69  40  19   6   7   8   9  26  51  84 125
+     * -2  107  70  41  20  21  22  23  24  25  50  83 124
+     * -3  108  71  42  43  44  45  46  47  48  49  82 123
+     * -4  109  72  73  74  75  76  77  78  79  80  81 122
+     * -5  110 111 112 113 114 115 116 117 118 119 120 121
+     */
+    public int obtenerNumero() {
+
+        final int absFila = Math.abs(fila);
+        final int absColumna = Math.abs(columna);
+        final int nivel = Math.max(absFila, absColumna);
+        final int desplazamiento;
+        final int coeficiente;
+
+        if (nivel == absFila) {
+            if (fila > 0) {
+                desplazamiento = -columna;
+                coeficiente = -1;
+            } else {
+                desplazamiento = columna;
+                coeficiente = 3;
+            }
+        } else {
+            if (columna > 0) {
+                desplazamiento = fila;
+                coeficiente = -3;
+            } else {
+                desplazamiento = -fila;
+                coeficiente = 1;
+            }
+        }
+
+        return nivel * (4 * nivel + coeficiente) + desplazamiento;
     }
 }
--- src/test/java/espacial/CoordenadasTest.java
+++ src/test/java/espacial/CoordenadasTest.java
@@ -3,69 +3,75 @@ package espacial;
 import espacial.test.Postcondicion;
 import espacial.test.TestDeContrato;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 class CoordenadasTest implements TestDeContrato {
 
     @Test
     void crearConFilaColumna() {
-        
+
         Coordenadas coordenadas = Coordenadas.con(5, 3);
-        
+
         comprobarQue(fueInicializada(coordenadas, 5, 3));
     }
 
     private Postcondicion fueInicializada(Coordenadas coordenadas, int fila, int columna) {
 
         return post(() -> {
-          
+
             assertThat(coordenadas.obtenerFila()).as("fila").isEqualTo(5);
             assertThat(coordenadas.obtenerColumna()).as("columna").isEqualTo(3);
         });
     }
-    
+
     @Test
     void sonIgualesSiSonElMismoObjeto() {
-        
+
         Coordenadas coordenadas = Coordenadas.con(2, 4);
-        
+
         assertThat(coordenadas.equals(coordenadas)).isTrue();
     }
-    
+
     @Test
     void sonIgualesSiCoincideFilaColumna() {
-        
+
         Coordenadas unaCoordenadas = Coordenadas.con(3, 9);
         Coordenadas otraCoordenadas = Coordenadas.con(3, 9);
-        
+
         assertThat(unaCoordenadas.equals(otraCoordenadas)).isTrue();
         assertThat(otraCoordenadas.equals(unaCoordenadas)).isTrue();
     }
-    
+
     @Test
     void noSonIgualesSiSoloCoincideFila() {
-        
+
         Coordenadas unaCoordenadas = Coordenadas.con(5, 8);
         Coordenadas otraCoordenadas = Coordenadas.con(5, -6);
-        
+
         assertThat(unaCoordenadas.equals(otraCoordenadas)).isFalse();
         assertThat(otraCoordenadas.equals(unaCoordenadas)).isFalse();
     }
-    
+
     @Test
     void noSonIgualesSiSoloCoincideLaColumna() {
-        
+
         Coordenadas unaCoordenadas = Coordenadas.con(2, 5);
         Coordenadas otraCoordenadas = Coordenadas.con(-1, 5);
-        
+
         assertThat(unaCoordenadas.equals(otraCoordenadas)).isFalse();
         assertThat(otraCoordenadas.equals(unaCoordenadas)).isFalse();
     }
-    
+
     @Test
     void siSonIgualesTieneElMismoHash() {
-        
+
         Coordenadas unaCoordenadas = Coordenadas.con(2, 5);
         Coordenadas otraCoordenadas = Coordenadas.con(2, 5);
 
@@ -78,5 +84,69 @@ class CoordenadasTest implements TestDeContrato {
         Coordenadas coordenadas = Coordenadas.con(23, 78);
 
         assertThat(coordenadas).hasToString("[23][78]");
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void obtenerNumero(int fila, int columna, int numero) {
+
+        Coordenadas coordenadas = Coordenadas.con(fila, columna);
+
+        assertThat(coordenadas.obtenerNumero()).as("numero de Coordenadas %s", coordenadas).isEqualTo(numero);
+    }
+
+    static Stream<Arguments> obtenerNumero() {
+
+        return Stream.of(
+                arguments(0, 1, 1),
+                arguments(1, 1, 2),
+                arguments(1, 0, 3),
+                arguments(1, -1, 4),
+                arguments(0, -1, 5),
+                arguments(-1, -1, 6),
+                arguments(-1, 0, 7),
+                arguments(-1, 1, 8),
+                arguments(-1, 2, 9),
+                arguments(0, 2, 10),
+                arguments(1, 2, 11),
+                arguments(2, 2, 12),
+                arguments(2, 1, 13),
+                arguments(2, 0, 14),
+                arguments(2, -1, 15),
+                arguments(2, -2, 16),
+                arguments(1, -2, 17),
+                arguments(0, -2, 18),
+                arguments(-1, -2, 19),
+                arguments(-2, -2, 20),
+                arguments(-2, -1, 21),
+                arguments(-2, 0, 22),
+                arguments(-2, 1, 23),
+                arguments(-2, 2, 24),
+                arguments(-2, 3, 25),
+                arguments(-1, 3, 26),
+                arguments(0, 3, 27),
+                arguments(1, 3, 28),
+                arguments(2, 3, 29),
+                arguments(3, 3, 30),
+                arguments(3, 2, 31),
+                arguments(3, 1, 32),
+                arguments(3, 0, 33),
+                arguments(3, -1, 34),
+                arguments(3, -2, 35),
+                arguments(3, -3, 36),
+                arguments(2, -3, 37),
+                arguments(1, -3, 38),
+                arguments(0, -3, 39),
+                arguments(-1, -3, 40),
+                arguments(-2, -3, 41),
+                arguments(-3, -3, 42),
+                arguments(-3, -2, 43),
+                arguments(-3, -1, 44),
+                arguments(-3, 0, 45),
+                arguments(-3, 1, 46),
+                arguments(-3, 2, 47),
+                arguments(-3, 3, 48),
+                arguments(0, 0, 0)
+        );
     }
 }
