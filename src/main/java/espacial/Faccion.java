e98699074918eafcda7527223005c76a3f8ef018
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-08-04 18:49:53 -0300

    Las Facciones soportan Condicionales

--- src/main/java/espacial/Faccion.java
+++ src/main/java/espacial/Faccion.java
@@ -1,6 +1,37 @@
 package espacial;
 
+import espacial.utiles.AccionSobre;
+
 public enum Faccion {
 
-    NEUTRAL, ALIADO
+    NEUTRAL(Condicional::siEsNeutral),
+    ALIADO(Condicional::siEsAliado),
+    RIVAL(Condicional::siEsRival);
+
+    private final AccionSobre<Condicional> alEvaluar;
+
+    Faccion(AccionSobre<Condicional> accionAlEvaluar) {
+
+        alEvaluar = accionAlEvaluar;
+    }
+
+    public void evaluar(Condicional condicional) {
+
+        alEvaluar.ejecutar(condicional);
+    }
+
+    public interface Condicional {
+
+        default void siEsNeutral() {
+
+        }
+
+        default void siEsAliado() {
+
+        }
+
+        default void siEsRival() {
+
+        }
+    }
 }
