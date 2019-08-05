3986048476b9a3767e9381feb45437238be50468
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-08-04 22:03:00 -0300

    Pinta las naves y las bases según sean aliados, rivales o neutrales

--- src/main/java/espacial/interfaz/componentes/DibujarBase.java
+++ src/main/java/espacial/interfaz/componentes/DibujarBase.java
@@ -1,5 +1,6 @@
 package espacial.interfaz.componentes;
 
+import espacial.Faccion;
 import espacial.Pieza;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -8,24 +9,26 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 
-public class DibujarBase extends Dibujar implements DibujarImagen {
+public class DibujarBase extends Dibujar implements DibujarImagen, Faccion.Condicional {
 
     private static final String CUBIERTA = "M26.242,18.541c-0.424,0-0.763,0.338-0.763,0.76v4.766l-1.411,1.41H19.26c-0.421,0-0.76,0.34-0.76,0.762v3.953h-2V26.24c0-0.422-0.339-0.762-0.76-0.762h-4.805l-1.415-1.412V19.26c0-0.422-0.339-0.76-0.76-0.76H4.485v-2h4.275c0.42,0,0.76-0.34,0.76-0.76v-4.807l1.415-1.414h4.805c0.42,0,0.76-0.338,0.76-0.76V4.807h2V8.76c0,0.422,0.339,0.76,0.76,0.76h4.809l1.408,1.414v4.807c0,0.42,0.34,0.76,0.763,0.76h4.275v2.041H26.242z";
 
-    private final Paint PINTURA = DibujarNave.PINTURA;
+    private Paint pintura = DibujarNave.PINTURA_FACCION_ALIADO;
 
     @Override
     public Node de(Pieza unaPieza) {
 
+        unaPieza.reconocer().evaluar(this);
+
         Rectangle casco = new Rectangle(4, 4, DibujarEspacio.DIMENSION - 8, DibujarEspacio.DIMENSION - 8);
-        casco.setFill(PINTURA);
+        casco.setFill(pintura);
         casco.setArcHeight(8);
         casco.setArcWidth(8);
         aplicarBorde(casco);
         aplicarSombraExteriorEn(casco);
 
         SVGPath cubierta = new SVGPath();
-        cubierta.setFill(PINTURA);
+        cubierta.setFill(pintura);
         cubierta.setBlendMode(BlendMode.MULTIPLY);
         cubierta.setContent(CUBIERTA);
         cubierta.setLayoutX(4);
@@ -34,5 +37,23 @@ public class DibujarBase extends Dibujar implements DibujarImagen {
         aplicarSombraInteriorEn(cubierta);
 
         return new Group(casco, cubierta);
+    }
+
+    @Override
+    public void siEsAliado() {
+
+        pintura = DibujarNave.PINTURA_FACCION_ALIADO;
+    }
+
+    @Override
+    public void siEsNeutral() {
+
+        pintura = DibujarNave.PINTURA_FACCION_NEUTRAL;
+    }
+
+    @Override
+    public void siEsRival() {
+
+        pintura = DibujarNave.PINTURA_FACCION_RIVAL;
     }
 }
