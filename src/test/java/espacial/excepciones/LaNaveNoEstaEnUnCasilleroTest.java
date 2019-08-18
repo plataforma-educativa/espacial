375af4ebc153d3471f3c4f508eaad7f459ac2090
Mariano Tugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2019-08-18 11:48:52 -0300

    Prueba mejorada para hacerla legible

--- src/test/java/espacial/excepciones/LaNaveNoEstaEnUnCasilleroTest.java
+++ src/test/java/espacial/excepciones/LaNaveNoEstaEnUnCasilleroTest.java
@@ -1,25 +1,49 @@
 package espacial.excepciones;
 
+import espacial.test.Postcondicion;
+import espacial.test.Precondicion;
+import espacial.test.TestDeContrato;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-class LaNaveNoEstaEnUnCasilleroTest {
+class LaNaveNoEstaEnUnCasilleroTest implements TestDeContrato {
+
+    ErrorEspacial unErrorEspacial;
 
     @Test
     void getMessage() {
 
-        assertThat(new LaNaveNoEstaEnUnCasillero())
-                .hasNoCause()
-                .hasMessage("La Nave no está en un Casillero del Tablero");
+        dadoQue(unErrorEspacialFueCreadoSinParametros());
+
+        comprobarQue(elMensajeEsDescriptivo());
+    }
+
+    private Precondicion unErrorEspacialFueCreadoSinParametros() {
+
+        return pre(condicion -> unErrorEspacial = new LaNaveNoEstaEnUnCasillero());
+    }
+
+    private Postcondicion elMensajeEsDescriptivo() {
+
+        return post(condicion -> assertThat(unErrorEspacial).hasMessage("La Nave no está en un Casillero del Tablero"));
     }
 
     @Test
     void getMessageIncluyeJustificacion() {
 
-        assertThat(new LaNaveNoEstaEnUnCasillero("porque no despegó de la Base"))
-                .hasNoCause()
-                .hasMessage("La Nave no está en un Casillero del Tablero porque no despegó de la Base");
+        dadoQue(fueCreadaConUnaJustificacion());
+
+        comprobarQue(incluyeLaJustificacionEnLaDescripcion());
     }
 
+    private Precondicion fueCreadaConUnaJustificacion() {
+
+        return pre(condicion -> unErrorEspacial = new LaNaveNoEstaEnUnCasillero("porque no despegó de la Base"));
+    }
+
+    private Postcondicion incluyeLaJustificacionEnLaDescripcion() {
+
+        return post(condicion -> assertThat(unErrorEspacial).hasMessage("La Nave no está en un Casillero del Tablero porque no despegó de la Base"));
+    }
 }
